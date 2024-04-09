6a8a1664e1a00917c01cf204db108da8a8b8c3da
Kapitoshka-a <148382009+Kapitoshka-a@users.noreply.github.com>
2024-04-09 10:30:22 +0300

    add chessboard size parameter

--- src/chess_knight.py
+++ src/chess_knight.py
@@ -1,19 +1,21 @@
 from typing import Tuple
 
-
-def is_valid(x, y):
-    return 0 <= x < 8 and 0 <= y < 8
+CHESSBOARD = 8
 
 
-def neighbors(row, col):
+def is_valid(x, y, chessboard=CHESSBOARD):
+    return 0 <= x < chessboard and 0 <= y < chessboard
+
+
+def neighbors(row, col, chessboard=CHESSBOARD):
     rows = [2, 2, -2, -2, 1, 1, -1, -1]
     cols = [-1, 1, 1, -1, 2, -2, 2, -2]
-    valid_neighbors = [(rows[i] + row, cols[i] + col) for i in range(8)
+    valid_neighbors = [(rows[i] + row, cols[i] + col) for i in range(chessboard)
                        if is_valid(rows[i] + row, cols[i] + col)]
     return valid_neighbors
 
 
-def shortest_knight_path(start_position: Tuple[int, int], end_position: Tuple[int, int]):
+def shortest_knight_path(start_position: Tuple[int, int], end_position: Tuple[int, int], chessboard=CHESSBOARD):
     row, col = start_position
     queue = [((row, col), 0)]
     visited = set()
@@ -21,7 +23,7 @@ def shortest_knight_path(start_position: Tuple[int, int], end_position: Tuple[in
     while queue:
         (row, col), distance = queue.pop(0)
         visited.add((row, col))
-        for row, col in neighbors(row, col):
+        for row, col in neighbors(row, col, chessboard):
             if (row, col) == end_position:
                 return distance + 1
             elif (row, col) not in visited:
--- tests/chess_knight_tests.py
+++ tests/chess_knight_tests.py
@@ -8,3 +8,6 @@ class TestChessKnight(unittest.TestCase):
 
     def test_case_2(self):
         self.assertEqual(shortest_knight_path((0, 0), (1, 2)), 1)
+
+    def test_case(self):
+        self.assertEqual(shortest_knight_path((0, 0), (1, 2), 2), None)
