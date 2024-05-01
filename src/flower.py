6757c5d09b039221731526752bf264d1b756962d
Kapitoshka-a <148382009+Kapitoshka-a@users.noreply.github.com>
2024-05-01 11:51:41 +0300

    correct code according to comments and add virtual vertex implementation

--- src/flower.py
+++ src/flower.py
@@ -1,16 +1,30 @@
+from typing import List, Set, Tuple
+
+DEFAULT_MATRIX_SIZE = 10
+DEFAULT_START_VIRTUAL_VERTEX = "START_VIRTUAL_VERTEX"
+DEFAULT_END_VIRTUAL_VERTEX = "END_VIRTUAL_VERTEX"
+
+
 class Graph:
-    def __init__(self, vertices: int = 10):
-        self.map_of_vertices = []
-        self.adj_matrix = [[0] * vertices for _ in range(vertices)]
+    def __init__(self, vertices: int = DEFAULT_MATRIX_SIZE):
+        self.list_of_vertices = []
+        self.edges_matrix = [[0] * vertices for _ in range(vertices)]
 
-    def add_edge(self, u, v, c):
-        if u not in self.map_of_vertices:
-            self.map_of_vertices.append(u)
-        if v not in self.map_of_vertices:
-            self.map_of_vertices.append(v)
-        self.adj_matrix[self.map_of_vertices.index(u)][self.map_of_vertices.index(v)] = c
+    def add_edge(self, current_vertex: str, next_vertex: str, weight: float):
+        if current_vertex not in self.list_of_vertices:
+            self.list_of_vertices.append(current_vertex)
+        if next_vertex not in self.list_of_vertices:
+            self.list_of_vertices.append(next_vertex)
+        self.edges_matrix[self.list_of_vertices.index(current_vertex)][
+            self.list_of_vertices.index(next_vertex)] = weight
 
-    def dfs(self, start_node, target_node, visited=None, path=None):
+    def add_virtual_vertexes(self, sources: list, sinks: list):
+        for vertex in sources:
+            self.add_edge(DEFAULT_START_VIRTUAL_VERTEX, vertex, float("inf"))
+        for vertex in sinks:
+            self.add_edge(vertex, DEFAULT_END_VIRTUAL_VERTEX, float("inf"))
+
+    def dfs(self, start_node: int, target_node: int, visited: Set[int] = None, path: List[int] = None):
         if visited is None:
             visited = set()
         if path is None:
@@ -21,29 +35,29 @@ class Graph:
 
         if start_node == target_node:
             return path
-        for ind, capacity in enumerate(self.adj_matrix[start_node]):
-            if ind not in visited and capacity > 0:
-                result_path = self.dfs(ind, target_node, visited, path.copy())
+        for index, capacity in enumerate(self.edges_matrix[start_node]):
+            if index not in visited and capacity > 0:
+                result_path = self.dfs(index, target_node, visited, path.copy())
                 if result_path:
                     return result_path
         return None
 
-    def ford_fulkerson(self, source, sink):
+    def ford_fulkerson(self, source: str, sink: str) -> int:
         max_flow = 0
-        source = self.map_of_vertices.index(source)
-        sink = self.map_of_vertices.index(sink)
+        source = self.list_of_vertices.index(source)
+        sink = self.list_of_vertices.index(sink)
 
         path = self.dfs(source, sink)
         while path:
             path_flow = float("Inf")
             for i in range(len(path) - 1):
-                u, v = path[i], path[i + 1]
-                path_flow = min(path_flow, self.adj_matrix[u][v])
+                current_vertex, next_vertex = path[i], path[i + 1]
+                path_flow = min(path_flow, self.edges_matrix[current_vertex][next_vertex])
 
             for i in range(len(path) - 1):
-                u, v = path[i], path[i + 1]
-                self.adj_matrix[u][v] -= path_flow
-                self.adj_matrix[v][u] += path_flow
+                current_vertex, next_vertex = path[i], path[i + 1]
+                self.edges_matrix[current_vertex][next_vertex] -= path_flow
+                self.edges_matrix[next_vertex][current_vertex] += path_flow
 
             max_flow += path_flow
 
@@ -51,22 +65,13 @@ class Graph:
 
         return max_flow
 
+    def max_flow(self, sources: list, sinks: list) -> int:
+        self.add_virtual_vertexes(sources, sinks)
+        return self.ford_fulkerson(DEFAULT_START_VIRTUAL_VERTEX, DEFAULT_END_VIRTUAL_VERTEX)
+
     def build_graph_from_csv(self, filename):
         with open(filename, 'r') as file:
             for line in file:
                 data = line.strip().split(',')
                 self.add_edge(data[0], data[1], int(data[2]))
-        return self.adj_matrix
-
-
-graph = Graph()
-graph.add_edge("F1", "F2", 10)
-graph.add_edge("F2", "S1", 5)
-graph.add_edge('F2', 'S1', 8)
-graph.add_edge('S2', 'S3', 12)
-graph.add_edge('S3', 'F2', 6)
-graph.add_edge('F2', 'S4', 15)
-graph.add_edge('F3', 'S5', 7)
-
-print(graph.ford_fulkerson("F1", "S4"))
-
+        return self.edges_matrix
--- tests/flower_tests.py
+++ tests/flower_tests.py
@@ -4,25 +4,21 @@ from src.flower import Graph
 
 class TestGraph(unittest.TestCase):
 
-    def test_add_edge(self):
-        graph = Graph(4)
-        graph.add_edge("A", "B", 5)
-        self.assertEqual(graph.adj_matrix, [[0, 5, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]])
-
     def test_dfs(self):
-        graph = Graph(4)
+        graph = Graph()
         graph.add_edge("A", "B", 5)
         graph.add_edge("B", "C", 10)
         graph.add_edge("C", "D", 15)
         self.assertEqual(graph.dfs(0, 3), [0, 1, 2, 3])
 
-    def test_ford_fulkerson(self):
-        graph = Graph(4)
+    def test_max_flow(self):
+        graph = Graph()
         graph.add_edge("A", "B", 5)
         graph.add_edge("B", "C", 10)
         graph.add_edge("C", "D", 15)
-        max_flow = graph.ford_fulkerson("A", "D")
-        self.assertEqual(max_flow, 5)
+        graph.add_edge("F", "K", 15)
+        max_flow = graph.max_flow(["A", "F"], ["D", "K"])
+        self.assertEqual(max_flow, 20)
 
     def test_build_graph_from_csv(self):
         graph = Graph(10)
@@ -38,7 +34,7 @@ class TestGraph(unittest.TestCase):
              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
-        self.assertEqual(graph.adj_matrix, expected_adj_matrix)
+        self.assertEqual(graph.edges_matrix, expected_adj_matrix)
 
 
 if __name__ == '__main__':
